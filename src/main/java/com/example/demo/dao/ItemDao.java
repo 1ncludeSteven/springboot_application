f25272710e31dacd5f4a300d45cc03693c62d5e9
steven <include@steven.com>
2022-09-08 21:26:22 +0800

    add ItemDao in dao package

---
+++ src/main/java/com/example/demo/dao/ItemDao.java
@@ -0,0 +1,29 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Item;
+
+import java.util.List;
+
+public class ItemDao {
+
+    public void addItem(Item item) {
+
+    }
+
+    public Item getItem(String id) {
+        return null;
+    }
+
+    public List<Item> getAllItems() {
+        return null;
+    }
+
+    public void updateItem(Item item) {
+
+    }
+
+    public void deleteItem(String id) {
+
+    }
+
+}
